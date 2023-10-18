d62f81ab3fcc9f0b09161e4733dc290db3655bcf
njordulv <njordulv@gmail.com>
2023-10-18 14:31:43 +0300

    ADD: functionality logic

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -1,28 +1,84 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import { PiEngine, PiListNumbersLight } from "react-icons/pi"
 import { IoCalendarNumberOutline, IoCarSport } from "react-icons/io5"
 import { FaHandHoldingDollar } from "react-icons/fa6"
+import queryString from "query-string"
 import carsData from "../data/carsData"
+import { useState, useEffect } from "react"
+
+const sortedKeys = ["year", "id", "make", "engine", "price"]
+
+const sortedCars = (cars, key) => {
+  const sortedArray = [...cars]
+  if (!key || !sortedKeys.includes(key)) {
+    return sortedArray
+  }
+  sortedArray.sort((a, b) => (a[key] > b[key] ? 1 : -1))
+  return sortedArray
+}
 
 const Cars = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
+  const query = queryString.parse(location.search)
+  const [sortKey, setSortKey] = useState(query.sort)
+  const [sortData, setSortData] = useState(sortedCars(carsData, sortKey))
+
+  useEffect(() => {
+    const updatedQuery = queryString.stringify({ sort: sortKey })
+    window.history.replaceState({}, "", `?${updatedQuery}`)
+    setSortData(sortedCars(carsData, sortKey))
+
+    if (!sortedKeys.includes(sortKey)) {
+      navigate(".")
+      setSortKey()
+    }
+  }, [sortKey, navigate])
+
+  const handlerSort = (key) => {
+    setSortKey(key)
+  }
+
   return (
     <div className="wrapper">
       <h1>Cars available</h1>
-      <div className="center">Sorted by </div>
+      <div className="center">
+        {sortKey ? `Sorted by ${sortKey}` : "Sorted by default"}
+      </div>
       <div className="buttons">
-        <button type="button">
+        <button
+          onClick={() => handlerSort("year")}
+          type="button"
+          className={sortKey === "year" ? "active" : ""}
+        >
           <IoCalendarNumberOutline />
         </button>
-        <button type="button">
+        <button
+          onClick={() => handlerSort("id")}
+          type="button"
+          className={sortKey === "id" ? "active" : ""}
+        >
           <PiListNumbersLight />
         </button>
-        <button type="button">
+        <button
+          onClick={() => handlerSort("make")}
+          type="button"
+          className={sortKey === "make" ? "active" : ""}
+        >
           <IoCarSport />
         </button>
-        <button type="button">
+        <button
+          onClick={() => handlerSort("engine")}
+          type="button"
+          className={sortKey === "engine" ? "active" : ""}
+        >
           <PiEngine />
         </button>
-        <button type="button">
+        <button
+          onClick={() => handlerSort("price")}
+          type="button"
+          className={sortKey === "price" ? "active" : ""}
+        >
           <FaHandHoldingDollar />
         </button>
       </div>
@@ -35,17 +91,23 @@ const Cars = () => {
         <span>Price</span>
       </div>
       <div className="car-items">
-        {carsData.map((car) => (
+        {sortData.map((car) => (
           <Link to={car.slug} key={car.id} className="item">
-            <span>{car.id}</span>
-            <span>{car.make}</span>
-            <span>{car.model}</span>
-            <span>{car.year}</span>
-            <span>
+            <span className={sortKey === "id" ? "active" : ""}>{car.id}</span>
+            <span className={sortKey === "make" ? "active" : ""}>
+              <b>{car.make}</b>
+            </span>
+            <span className={sortKey === "model" ? "active" : ""}>
+              <b>{car.model}</b>
+            </span>
+            <span className={sortKey === "year" ? "active" : ""}>
+              {car.year}
+            </span>
+            <span className={sortKey === "engine" ? "active" : ""}>
               {car.engine}
               <i>l.</i>
             </span>
-            <span>
+            <span className={sortKey === "price" ? "active" : ""}>
               <i>$</i>
               {car.price}
             </span>
